deef7ecebaba71ed8cbfc6f5cb59f231c26b095e
argest <isakovalihan777@gmail.com>
2022-05-03 12:25:54 +0300

    Creating a layer DAL, Domain and adding the News entity.

---
+++ ProjectMicron/ProjectMicron.Domain/Entity/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMicron.Domain.Entity
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public string Owner { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+      
+    }
+}
---
+++ ProjectMicron/ProjectMicron.Domain/Entity/NewsRobot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectMicron.Domain.Entity
+{
+    public class NewsRobot
+    {
+
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreationTime { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+
+        public ICollection<User> Users { get; set; }
+
+    }
+}
